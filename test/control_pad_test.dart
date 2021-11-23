455dbfe93b0610fc6825d28e0b9946f2251d8e85
rjvysakh <vysakh.rj@neoito.com>
2021-11-23 12:54:33 +0530

    fix: added test

--- test/control_pad_test.dart
+++ test/control_pad_test.dart
@@ -1,7 +1,12 @@
 // import 'package:flutter_test/flutter_test.dart';
 // import 'package:control_pad/control_pad.dart';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
-  // test('adds one to input values', () {
-  // });
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp());
+  });
 }
